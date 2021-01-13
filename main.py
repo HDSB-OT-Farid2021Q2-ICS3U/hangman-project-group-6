7eab51e4c3efcb821be7da634b8f4f794e07ffb3
AustinHamilton1 <1hamiltonaus@hdsb.ca>
2021-01-13 19:45:21 +0000

    changes

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import random #Importing random
 
-
-#Word bank fille with many words
-def wordbank ():
-    words = ['butterball','elephant','tree','computer','bank','apple','house','orange','Albania','Armenia','Austria','Belgium','Bulgaria','Lamborghini','Toyota','Mercedes','Honda','Nissan','Chile','Pear','Calculater','Oven','Cat','Llizzard','Iguana','Google','Phone','Wheels','Mouse','Lion','Leafs','Montreal','Toronto','California','Stump','Japan','Tokyo','Fast','Furious','Supra','super','Skyline','dinosaur','Case','Dirtbike','Yamaha','Mercury','Volvo','Suzuki','Boat','buffalo','supreme']
-
-    randWord = random.choice(words) #Assiogning a random word to this variable so that Tk can use it when deciding in the word
-
-    print(random.choice(words)) #printintg im not sure if this is needed but its here anyways
-
-wordbank()#Calling word bank
