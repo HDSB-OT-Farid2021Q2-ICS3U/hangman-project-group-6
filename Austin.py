492de070de11235f5c056678f6a988fd46deb829
phamtrantuankha <phamtrantuankhanh2309@gmail.com>
2021-01-14 01:53:46 +0000

    Partially done for now

--- Austin.py
+++
@@ -1,6 +0,0 @@
-def wordbank ():
-    words = ['butterball','elephant','tree','computer','bank','apple','house','orange','Albania','Armenia','Austria','Belgium','Bulgaria','Lamborghini','Toyota','Mercedes','Honda','Nissan','Chile','Pear','Calculater','Oven','Cat','Llizzard','Iguana','Google','Phone','Wheels','Mouse','Lion','Leafs','Montreal','Toronto','California','Stump','Japan','Tokyo','Fast','Furious','Supra','super','Skyline','dinosaur','Case','Dirtbike','Yamaha','Mercury','Volvo','Suzuki','Boat','buffalo']
-
-    print(words)
-
-wordbank()
--- tk.py
+++ tk.py
@@ -1,3 +1,25 @@
+import random
+import os
 
+def clear():
+  os.system("clear")
+
+words = ['butterball','elephant','tree','computer','bank','apple','house','orange',
+         'Albania','Armenia','Austria','Belgium','Bulgaria','Lamborghini','Toyota',
+         'Mercedes','Honda','Nissan','Chile','Pear','Calculater','Oven','Cat','Llizzard','Iguana',
+         'Google','Phone','Wheels','Mouse','Lion','Leafs','Montreal','Toronto','California',
+         'Stump','Japan','Tokyo','Fast','Furious','Supra','super','Skyline','dinosaur','Case',
+         'Dirtbike','Yamaha','Mercury','Volvo','Suzuki','Boat','buffalo'
+        ]
 
 guesses = 6
+
+
+word = random.choice(words)
+progress = "-" * len(word)
+while guesses > 0:
+  guess = input("Enter a letter: ")
+  if guess in word:
+    print()
+  else:
+    guesses -= 1
