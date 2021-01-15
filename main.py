6c7d15d293939a09d8735915832b246c9f353ea2
AustinHamilton1 <1hamiltonaus@hdsb.ca>
2021-01-15 18:26:39 +0000

    Tks

--- main.py
+++ main.py
@@ -119,3 +119,30 @@ def wordbank ():
 
 wordbank()#Calling word bank
 
+import os
+import time
+
+def clear():
+  os.system("clear")
+
+guesses = 6
+word = random.choice(words)
+word = list(word)
+
+while guesses > 0:
+  hidden_word = [print("-", end = "") for x in word]
+  guess = input("\nEnter a letter: ")
+  if guess in word:
+    print("You have guessed a correct letter!")
+    if guess in hidden_word: hidden_word.replace(guess)
+  else:
+    guesses -= 1
+    print("Sorry, you have entered an incorrect letter.")
+    print("You have {} guesses left.".format(guesses))
+    time.sleep(3)
+    clear()
+  if guesses == 0:
+    word = [print(x) for x in word]
+    print("Sorry, the word was \"{}\"".format(word))
+    print("Better luck next time!")
+
