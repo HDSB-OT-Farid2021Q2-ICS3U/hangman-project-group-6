bdbe8df5559ae3a56534c2981718f2c00bce0eeb
phamtrantuankha <phamtrantuankhanh2309@gmail.com>
2021-01-15 17:53:56 +0000

    Help

--- tk.py
+++ tk.py
@@ -1,25 +1,34 @@
+
 import random
 import os
+import time
 
 def clear():
   os.system("clear")
 
-words = ['butterball','elephant','tree','computer','bank','apple','house','orange',
-         'Albania','Armenia','Austria','Belgium','Bulgaria','Lamborghini','Toyota',
-         'Mercedes','Honda','Nissan','Chile','Pear','Calculater','Oven','Cat','Llizzard','Iguana',
-         'Google','Phone','Wheels','Mouse','Lion','Leafs','Montreal','Toronto','California',
-         'Stump','Japan','Tokyo','Fast','Furious','Supra','super','Skyline','dinosaur','Case',
-         'Dirtbike','Yamaha','Mercury','Volvo','Suzuki','Boat','buffalo'
+words = ['butterball','elephant','tree','computer','bank','apple',
+         'house','orange','Albania','Armenia','Austria','Belgium',
+         'Bulgaria','Lamborghini','Toyota','Mercedes','Honda',
+         'Nissan','Chile','Pear','Calculator','Oven','Cat',
+         'Llizzard','Iguana','Google','Phone','Wheels','Mouse',
+         'Lion','Leafs','Montreal','Toronto','California','Stump',
+         'Japan','Tokyo','Fast','Furious','Supra','super','Skyline',
+         'dinosaur','Case','Dirtbike','Yamaha','Mercury','Volvo',
+         'Suzuki','Boat','buffalo'
         ]
 
 guesses = 6
-
-
 word = random.choice(words)
-progress = "-" * len(word)
+word = list(word)
+hidden_word = [print("-", end = "") for x in word]
+
 while guesses > 0:
-  guess = input("Enter a letter: ")
+  guess = input("\nEnter a letter: ")
   if guess in word:
-    print()
+    print("You have guessed a correct letter!")
   else:
     guesses -= 1
+    print("Sorry, you have entered an incorrect letter.")
+    print("You have {} guesses left.".format(guesses))
+    time.sleep(3)
+    clear()
